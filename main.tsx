d6e1fd5ef6f27214542e174ff0fb74c396950343
edymusajev <edymusajev@gmail.com>
2025-02-07 14:36:01 +0700

    experiment

--- main.tsx
+++ main.tsx
@@ -1,20 +1,64 @@
-import React from "react";
 import { PluginContext } from "apyf";
-// import { CatalogBrowser } from "./src/CatalogBrowser";
+import React, { useState } from "react";
+
+const BirdGame = (props: { pluginContext: PluginContext }) => {
+  const [counter, setCounter] = useState(0);
+  return (
+    <div>
+      <h1>Bird Game</h1>
+      <div>{JSON.stringify(props)}</div>
+      <button
+        onClick={() => {
+          setCounter(counter + 1);
+          console.log(props);
+        }}
+      >
+        Click me
+      </button>
+      <div>{counter}</div>
+    </div>
+  );
+};
 
 const fusionApiPlugin = (context: PluginContext) => {
   return {
     onload: () => {
-      console.log("SHOULD BE RIGHT");
+      const BirdGameWithContext = (props: any) => (
+        <BirdGame {...props} pluginContext={context} />
+      );
 
-      const catalogBrowserTab = {
-        id: "catalog-browser",
-        icon: "smile",
-        title: "Catalog Browser",
-        content: <div>hello 123</div>,
+      const testPanel = {
+        id: "test-panel",
+        icon: "gamepad-2",
+        title: "Test Panel",
+        content: (
+          <div className="p-2 space-y-2">
+            <button
+              onClick={() => {
+                context.addTab("main", {
+                  id: "test-panel-2",
+                  title: "Test Panel 2",
+                  icon: null,
+                  props: {
+                    content: "coming soon",
+                  },
+                });
+              }}
+              className="rounded bg-green-600 p-2"
+            >
+              And another one
+            </button>
+          </div>
+        ),
       };
+      context.registerSidebarTab("left", testPanel);
 
-      context.registerSidebarTab("left", catalogBrowserTab);
+      context.registerPanel("main", {
+        id: "test-panel",
+        icon: "heart",
+        title: "👾 Minigame",
+        content: BirdGameWithContext,
+      });
     },
     onunload: () => {
       console.log("hello plugin unloaded");
